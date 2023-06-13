49023d1bf435d0c1e47e450410c1d709fa16fc74
Abdalla Suliman <abdalla@MacOSs-MacBook-Pro.local>
2023-06-13 22:09:37 +0800

    add stying for pokemon card

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
   useEffect(() => {
     const fetchPokemonList = async () => {
       try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=5');
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
         setPokemonList(response.data.results);
       } catch (error) {
         console.error('Error fetching Pokémon list:', error);
@@ -53,16 +53,17 @@ const Home = () => {
   }, [pokemonList]);
   console.log(pokemonList)
   return (
-    <div className="container mx-auto">
+    <>
+    <div className="container mx-auto p-2 justify-center">
       <h1 className="text-2xl font-bold text-center mt-8">Pokémon List</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-8">
         {pokemonDetails.map((pokemon) => (
           <div key={pokemon.name} className="border border-gray-300 p-4 rounded">
-            <img src={pokemon.image} alt={pokemon.name} className="w-24 h-24 mx-auto mb-4" />
-            <h2 className="text-lg font-bold text-center">{pokemon.name}</h2>
-            <ul className="mt-2">
+            <img src={pokemon.image} alt={pokemon.name} className="object-cover h-48 w-48 mx-auto mb-4" />
+            <h2 className="text-lg font-bold text-left">{pokemon.name}</h2>
+            <ul className="flex gap-2 mt-2">
               {pokemon.traits.map((trait, index) => (
-                <li key={index} className="text-center">
+                <li key={index} className="text-center bg-gray-200 px-4 py-2 rounded-lg	">
                   {trait}
                 </li>
               ))}
@@ -71,6 +72,7 @@ const Home = () => {
         ))}
       </div>
     </div>
+    </>
   )
 }
 
