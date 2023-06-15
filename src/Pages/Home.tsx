7bfa96628a5f9afc45702ac9cef4c2ba356a7606
Abdalla Suliman <abdalla@MacOSs-MacBook-Pro.local>
2023-06-15 08:26:34 +0800

    remove debugging lines

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -36,18 +36,6 @@ const Home = () => {
     fetchPokemonList();
   }, [currentPage, resultsPerPage]);
 
-  // useEffect(() => {
-  //   const fetchPokemonList = async () => {
-  //     try {
-  //       const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
-  //       setPokemonList(response.data.results);
-  //     } catch (error) {
-  //       console.error('Error fetching Pokémon list:', error);
-  //     }
-  //   };
-
-  //   fetchPokemonList();
-  // }, []);
 
   useEffect(() => {
     const fetchPokemonDetails = async () => {
